b7fafc3835b617f1bd475e206c6d9a2ed93ca926
Richard Borges <ribborges@outlook.com>
2022-12-26 00:07:42 -0300

    Add Alura Studies to portfolio

--- src/components/section/Portfolio.tsx
+++ src/components/section/Portfolio.tsx
@@ -14,7 +14,7 @@ export default function About() {
 			title={"Social tree"}
 			text={"Rocketseat social tree challenge"}>
 				<Button href="https://socialtree-richardborges.vercel.app/"><i className="bi bi-box-arrow-up-right"></i> Demo</Button>
-				<Button href="https://github.com/ribborges/social-tree"><i className="bi bi-github"></i> Code</Button>
+				<Button href="https://github.com/ribborges/social-tree/"><i className="bi bi-github"></i> Code</Button>
 			</Card>
 
 			<Card
@@ -22,7 +22,15 @@ export default function About() {
 			title={"Portfolio"}
 			text={"Rocketseat portfolio challenge"}>
 				<Button href="https://ribborges.github.io/rocketseat-challenges/portfolio/"><i className="bi bi-box-arrow-up-right"></i> Demo</Button>
-				<Button href="https://github.com/ribborges/rocketseat-challenges/tree/main/portfolio"><i className="bi bi-github"></i> Code</Button>
+				<Button href="https://github.com/ribborges/rocketseat-challenges/tree/main/portfolio/"><i className="bi bi-github"></i> Code</Button>
+			</Card>
+
+			<Card
+			thumb={"/static/img/projects/aluraStudies.png"}
+			title={"Alura Studies"}
+			text={"Studie list with stopwatch"}>
+				<Button href="https://alura-studies-gray.vercel.app/"><i className="bi bi-box-arrow-up-right"></i> Demo</Button>
+				<Button href="https://github.com/ribborges/alura-studies/"><i className="bi bi-github"></i> Code</Button>
 			</Card>
 
 			<Card
@@ -56,10 +64,6 @@ export default function About() {
 				<Button href="https://ribborges.github.io/Kids/"><i className="bi bi-box-arrow-up-right"></i> Demo</Button>
 				<Button href="https://github.com/ribborges/Kids"><i className="bi bi-github"></i> Code</Button>
 			</Card>
-		</div>
-
-		<div className="flex-container">
-			
 		</div>
 	</section>
   )
