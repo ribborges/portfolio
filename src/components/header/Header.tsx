5d6e0cea2c16f0b422484e88642dd7209372d2e4
Richard Borges <ribborges@outlook.com>
2023-01-10 14:40:13 -0300

    Small header changes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,8 +3,9 @@ import './_style.scss';
 export default function Header() {
   return (
     <header className="header">
-		<div className="avatar">
-			<img src="https://avatars.githubusercontent.com/u/46366493"/>
+		<div>
+			<h3>Hi there 🫡</h3>~
+			<h1>My name is Richard de Carvalho Borges</h1>
 		</div>
 		<div>
 			<h1 className="title">Front-end <br/>developer</h1>
