63adbaedfab82b7cb121eb79632db6aba9c7c11a
Richard <ribborges@outlook.com>
2024-03-13 17:56:32 -0300

    Add Cook App project

--- src/components/section/portfolio/Full.tsx
+++ src/components/section/portfolio/Full.tsx
@@ -6,12 +6,29 @@ import ButtonLink from "../../input/ButtonLink";
 import { BoxArrowUpRight, Github, ThreeDotsVertical } from "react-bootstrap-icons";
 
 interface portfolioProps {
-	lang?: displayText,
+    lang?: displayText,
 }
 
 export default function Full(props: portfolioProps) {
     return (
         <Grid>
+            <Card
+                thumb="/static/img/projects/cookApp.png"
+                title={"Cook App"}
+                text={`
+                    Projeto desenvolvido durante o evento "React Native na prática" da Rocketseat.
+
+                    Imagine chegar em casa com fome depois de um dia de muito trabalho e lembrar que você esqueceu de 
+                    passar no mercado para fazer as compras da semana. Então você decide abrir a geladeira pra fazer 
+                    uma janta com o que já tem em casa, mas está sem muita criatividade pra cozinhar. É esse problema 
+                    que o Cook App pode resolve! Nessa aula do evento React Native na prática vamos criar uma aplicação 
+                    que sugere receitas conforme os ingredientes selecionados usando React Native, Expo, Expo router, 
+                    Supabase e React Native reanimated.
+                `}
+            >
+                <ButtonLink href="https://github.com/ribborges/cook-app/tree/master"><Github /> {props.lang?.portfolio[1].project[1].btn[0]}</ButtonLink>
+            </Card>
+
             <Card
                 thumb="/static/img/projects/fisioHome.png"
                 title={props.lang?.portfolio[1].project[0].title}
