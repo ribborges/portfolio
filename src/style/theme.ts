aad54d31d0ba91fb1d4b8bb4c34c5a244adb6cbf
Richard <ribborges@outlook.com>
2024-04-12 01:08:43 -0300

    chore: add new theme variables

---
+++ src/style/theme.ts
@@ -0,0 +1,21 @@
+const font = {
+    family: "'Ubuntu', sans-serif"
+};
+
+const color = {
+    accent_primary: "#4a01ff",
+    accent_secondary: "#d04cff",
+    accent_tertiary: "#ff4197",
+    dark: "#0e0e0e",
+    dark_grey: "#222222",
+    light_grey: "#dddddd",
+    light: "#eeeeee"
+};
+
+const radius = {
+    small: "5px",
+    medium: "10px",
+    large: "20px"
+};
+
+export { font, color, radius};
