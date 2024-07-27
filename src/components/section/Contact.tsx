2d45f1718f4d96b89a2f68212f5ff8a0ab88f00d
Richard <ribborges@outlook.com>
2024-07-27 15:22:54 -0300

    style: update cards and buttons styles

--- src/components/section/Contact.tsx
+++ src/components/section/Contact.tsx
@@ -9,9 +9,9 @@ export default function Contact() {
       <h1><i className="bi bi-list-task"></i> CONTACT ME</h1>
       <Blanckspace />
       <div className="flex-container flex-col">
-        <ButtonLink href="mailto:ribborges@outlook.com" className="accent big"><EnvelopeFill/> Email</ButtonLink>
-        <ButtonLink href="https://linkedin.com/in/ribborges" className="accent big"><Linkedin /> LinkedIn</ButtonLink>
-        <ButtonLink href="https://github.com/ribborges" className="accent big"><Github /> Github</ButtonLink>
+        <ButtonLink href="mailto:ribborges@outlook.com" className="accent big" icon={<EnvelopeFill/>} label="Email"/>
+        <ButtonLink href="https://linkedin.com/in/ribborges" className="accent big" icon={<Linkedin/>} label="LinkedIn"/>
+        <ButtonLink href="https://github.com/ribborges" className="accent big" icon={<Github/>} label="Github"/>
       </div>
     </section>
   );
