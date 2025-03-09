b613281f9406cc67e2dc1a8f76b49b767139b28b
Richard Borges <ribborges@outlook.com>
2025-03-09 15:25:17 -0300

    feat: refactor Separator component to use clsx for class management and enhance spacing props

--- src/components/Separator/index.tsx
+++ src/components/Separator/index.tsx
@@ -1,27 +1,29 @@
-'use client';
-
-import classConcat from '../../utils/classConcat';
+import { clsx } from 'clsx';
 
 interface SeparatorProps {
-    height?: number;
+    space?: number;
+    vertical?: boolean;
+    className?: string;
 }
 
-function Spacer({ height = 120 }: SeparatorProps) {
-    return <hr className={
-        classConcat(
-            `mt-[${height / 2}px] mb-[${height / 2}px]`,
-            "border-t-1 border-solid border-zinc-300",
-        )
-    } />;
+function Spacer({ space = 120, vertical, className }: SeparatorProps) {
+    return <hr
+        style={vertical ? { marginRight: `${space / 2}px`, marginLeft: `${space / 2}px` } : { marginTop: `${space / 2}px`, marginBottom: `${space / 2}px` }}
+        className={clsx(
+            "box-border border-solid border-zinc-200 dark:border-zinc-800",
+            vertical ? "h-full border-r" : "w-full border-t",
+            className || ""
+        )} />;
 }
 
-function Blanckspace({ height = 60 }: SeparatorProps) {
-    return <hr className={
-        classConcat(
-            `h-[${height}px]`,
+function Blanckspace({ space = 60, vertical, className }: SeparatorProps) {
+    return <hr
+        style={vertical ? { width: `${space}px` } : { height: `${space}px` }}
+        className={clsx(
             "border-none",
-        )
-    } />;
+            className || ""
+        )}
+    />;
 }
 
 export { Spacer, Blanckspace };
