e738b5493541c235f20394d9eef4fb2f49979cd7
Richard Borges <ribborges@outlook.com>
2025-03-09 15:23:30 -0300

    chore: update to tailwindcss 4; remove unnecessary files and configs

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@
 const nextConfig = {
   output: 'export', // Outputs a Single-Page Application (SPA).
   distDir: './dist', // Changes the build output directory to `./dist/`.
-  compiler: {
-    styledComponents: true,
-  },
 };
 
 export default nextConfig;
