1c08024e62147894f54165636efcccffa92f789b
Richard Borges <ribborges@outlook.com>
2025-03-10 00:06:24 -0300

    feat: update Header component to use H1 and H2 for improved semantics and adjust layout styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { EnvelopeFill, Linkedin, Github, FileEarmarkPersonFill } from "react-boo
 
 import ButtonLink from '@/components/Input/Button';
 import { Blanckspace } from '@/components/Separator';
+import { H1, H2 } from "@/components/Heading";
 
 import "./style.css";
 
@@ -12,22 +13,24 @@ export default function Header() {
 			header-bg
 			flex items-center justify-between flex-col-reverse
 			md:flex-row
-		"
-			id="header"
-		>
-			<div className="flex-1 m-5">
+			p-6 md:p-12 lg:p-24
+		" id="header">
+			<div className="flex-1">
 				<div>
-					<h3>Olá</h3>
-					<h1>Sou <span className="
-						font-bold
-						p-1
-						bg-zinc-950 text-zinc-100 dark:bg-zinc-100 dark:text-zinc-950 hover:bg-indigo-700
-						transition duration-500
-					">RICHARD BORGES</span></h1>
-					<h2>Desenvolvedor Full Stack e Engenheiro de Software</h2>
+					<span className="font-bold text-lg md:text-xl lg:text-2xl">Olá</span>
+					<H1 className="justify-start flex-wrap">
+						<span>Sou</span>
+						<span className="
+							font-bold
+							px-1
+							bg-zinc-950 text-zinc-100 dark:bg-zinc-100 dark:text-zinc-950 hover:bg-indigo-700
+							transition duration-500
+						">RICHARD BORGES</span>
+					</H1>
+					<H2>Engenheiro de Software</H2>
 				</div>
 				<Blanckspace />
-				<div className="flex flex-wrap">
+				<div className="flex flex-wrap justify-center md:justify-start gap-2">
 					<ButtonLink href="mailto:contato@richardborges.dev" icon={<EnvelopeFill />} label="Email" />
 					<ButtonLink href="https://linkedin.com/in/ribborges" icon={<Linkedin />} label="Linkedin" />
 					<ButtonLink href="https://github.com/ribborges" icon={<Github />} label="Github" />
@@ -37,11 +40,11 @@ export default function Header() {
 			<div className="
 				flex
 				overflow-hidden
-				max-w-96 max-h-96
+				h-52 w-52 md:h-72 md:w-72 lg:h-96 lg:w-96
 				border-2 border-solid border-zinc-300 dark:border-zinc-800 border-opacity-50
 				rounded-[50%] hover:rounded-3xl
 				mt-24 md:mt-0
-				transition-all duration-1000
+				transition-all duration-500
 			">
 				<img className="grayscale hover:grayscale-0 transition duration-1000" src="/static/img/avatar.webp" />
 			</div>
