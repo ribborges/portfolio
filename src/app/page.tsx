1420e5d10d054f060ebb748ada8fadd71aec9a21
Richard Borges <ribborges@outlook.com>
2025-03-10 00:03:01 -0300

    feat: update page layout with Blanckspace component and adjust Spacer usage; clean up global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { BriefcaseFill, HouseDoorFill, PersonFill } from "react-bootstrap-icons"
 import { NavBar } from "@/components/Navbar";
 import Header from "@/components/Header";
 import About from "@/components/section/About";
-import { Spacer } from "@/components/Separator";
+import { Blanckspace, Spacer } from "@/components/Separator";
 import Portfolio from "@/components/section/Portfolio";
 import Footer from "@/components/Footer";
 import { MenuLink } from "@/components/Input";
@@ -16,11 +16,14 @@ export default function Home() {
         <MenuLink icon={<PersonFill />} label="Sobre mim" href="#about" />
         <MenuLink icon={<BriefcaseFill />} label="Portfolio" href="#portfolio" />
       </NavBar>
+      
       <Header/>
 
-      <main className="content">
+      <Blanckspace space={120} />
+
+      <main className="p-2 md:p-4 lg:p-16">
         <About />
-        <Spacer />
+        <Spacer space={240} />
         <Portfolio />
       </main>
 
