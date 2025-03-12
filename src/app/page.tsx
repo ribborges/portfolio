432085c6153d2ea2a25d1ca3b0fe2b8cff313ba0
Richard Borges <ribborges@outlook.com>
2025-03-11 21:30:30 -0300

    feat: add Contact section and link in NavBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { BriefcaseFill, HouseDoorFill, PersonFill } from "react-bootstrap-icons";
+import { BriefcaseFill, HouseDoorFill, PersonFill, SendFill } from "react-bootstrap-icons";
 
 import { NavBar } from "@/components/Navbar";
 import Header from "@/components/Header";
@@ -7,6 +7,7 @@ import { Blanckspace, Spacer } from "@/components/Separator";
 import Portfolio from "@/components/section/Portfolio";
 import Footer from "@/components/Footer";
 import { MenuLink } from "@/components/Input";
+import Contact from "@/components/section/Contact";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <MenuLink icon={<HouseDoorFill />} label="Inicio" href="#header" />
         <MenuLink icon={<PersonFill />} label="Sobre mim" href="#about" />
         <MenuLink icon={<BriefcaseFill />} label="Portfolio" href="#portfolio" />
+        <MenuLink icon={<SendFill />} label="Contato" href="#contact" />
       </NavBar>
       
       <Header/>
@@ -25,6 +27,8 @@ export default function Home() {
         <About />
         <Spacer space={240} />
         <Portfolio />
+        <Spacer space={240} />
+        <Contact />
       </main>
 
       <Footer />
